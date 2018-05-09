c931b48be936c6d76a0ce7f364d1a25d12c8ae5b
artemlunev <lampard95@gmail.com>
2018-05-09 14:30:45 +0300

    bot off

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import config
 import sql_lightlog
 import schedule
 
-device = 'string'
+
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(regexp="status")
@@ -18,15 +18,6 @@ def send_status(message):
         bot.send_message(message.chat.id, mess)
         print(mess)
 
-@bot.message_handler(regexp="add")
-def add(message):
-    #Эти параметры для клавиатуры необязательны, просто для удобства
-        keyboard = types.InlineKeyboardMarkup(row_width=1, resize_keyboard=True)
-        button_lamp = types.InlineKeyboardButton(text="Лампа", callback_data="lamp")
-        button_pump = types.InlineKeyboardButton(text="Насос", callback_data="pump")
-        button_all = types.InlineKeyboardButton(text="Всё", callback_data="all")
-        keyboard.add(button_lamp, button_pump, button_all)
-        bot.send_message(message.chat.id, "Что включить?", reply_markup=keyboard)
 
 @bot.message_handler(regexp="lamp 1")
 def lamp_on(message):
@@ -45,18 +36,6 @@ def lamp_on(message):
         schedule.add(2, 'pump')
 
 
-@bot.callback_query_handler(func=lambda c: True)
-def inline(c):
-        global device
-        if c.data == 'lamp':
-                device = 'lamp'
-        if c.data == 'pump':
-                device = 'pump'
-        if c.data == 'all':
-                device = 'all'
-        keyboard = types.InlineKeyboardMarkup(row_width=1, resize_keyboard=True)
-        keyboard.add(*[types.InlineKeyboardButton(text=time, callback_data=time) for time in ['1', '2', '3', '4', '5']])
-        bot.send_message(message.chat.id, "На какое время?", reply_markup=keyboard)
 
 
 
--- bot_post.py
+++ bot_post.py
@@ -1,8 +1,8 @@
 import telebot
 import config
 
-bot = telebot.TeleBot(config.token)
+#bot = telebot.TeleBot(config.token)
 
 def send_to_farm(text):
-        bot.send_message(config.telegram_chat_number, text)
+#        bot.send_message(config.telegram_chat_number, text)
         print(text)
